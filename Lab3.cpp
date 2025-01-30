a84cfb68d8839fa953dbbd4ea30df9324632842d
SirOboldui <124663090+SirOboldui@users.noreply.github.com>
2025-01-30 22:27:13 +0200

    Add files via upload

---
+++ Lab3.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+bool sprchk(char a, char* separ)
+{
+    //char* separ = separators;
+    while(*separ != '\0')
+    {
+        if (a == *separ)
+        {
+            return true;
+        }
+        separ++;
+    }
+    return false;
+}
+void bufins(int &bc, int &ws, char* arr, char* buf)
+{
+    for (int k = bc - 1; k >= 0; k--)
+    {
+        arr[ws] = buf[k];
+        ws++;
+    }
+    bc = 0;
+}
+
+int main()
+{
+    char ustr[500], finstr[500], buf[500];
+    cout << "Enter something:\n";
+    cin.getline(ustr, 500, '\n');
+    char separ[] = ",;.':@!#$%^&*()_+= ~[]{}|/";
+
+    bool word = false, rev = false;
+    int j = 0,bk =0;
+
+    for (int i = 0; i < strlen(ustr); i++)
+    {
+        if (sprchk(ustr[i], separ) == true)
+        {
+            if (word == true)
+            {
+                if (rev == true)
+                {
+                    bufins(bk, j, finstr, buf);
+                }
+                rev = not rev;
+            }
+            finstr[j] = ustr[i];
+            j++;
+            word = false;
+        }
+        else
+        {
+            word = true;
+            if (rev == true)
+            {
+                buf[bk] = ustr[i];
+                bk++;
+            }
+        }
+        
+    }
+    if (bk != 0)
+        bufins(bk, j, finstr, buf);
+    for (int k = 0; k <= j-1; k++)
+        cout << finstr[k];
+}
+
+/*
+Test1
+
+Good morning
+ gninrom
+
+Test2
+
+    hello; my friend
+    ; ym
+
+Test3
+
+Big apl:le
+ lpa:
+*/
